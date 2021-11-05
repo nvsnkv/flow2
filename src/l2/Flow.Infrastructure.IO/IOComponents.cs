cf39b92a6802cb9e4af5b1e4388785c806e45e9a
Nikita Senkov <nvsnkv@live.com>
2021-11-05 21:55:25 +0300

    update io

---
+++ src/l2/Flow.Infrastructure.IO/IOComponents.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Autofac;
+using CsvHelper.Configuration;
+using Flow.Infrastructure.Configuration.Contract;
+using Newtonsoft.Json;
+
+namespace Flow.Infrastructure.IO;
+
+public class IOComponents : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.Register(c =>
+        {
+            var config = c.Resolve<IFlowConfiguration>();
+
+            var culture = CultureInfo.GetCultures(CultureTypes.AllCultures)
+                              .FirstOrDefault(c => c.Name == config.CultureCode)
+                          ?? CultureInfo.CurrentCulture;
+
+            var csv = new CsvTransactionsSerializer(new CsvConfiguration(culture));
+            var json = new JsonTransactionsSerializer(new JsonSerializerSettings() { Culture = culture });
+            return new TransactionsIOFacade(csv, json);
+        }).InstancePerLifetimeScope().AsImplementedInterfaces();
+
+        base.Load(builder);
+    }
+}
---
+++ src/l2/Flow.Infrastructure.Storage/FlowDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Autofac;
+using Flow.Infrastructure.Configuration.Contract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flow.Infrastructure.Storage;
+
+public class FlowDatabase : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.Register(c =>
+        {
+            var config = c.Resolve<IFlowConfiguration>();
+            return new FlowDbContextFactory(config);
+        }).InstancePerLifetimeScope().AsImplementedInterfaces();
+
+        builder.Register(c => new TransactionStorage(c.Resolve<IDbContextFactory<FlowDbContext>>())).InstancePerLifetimeScope().AsImplementedInterfaces();
+
+        base.Load(builder);
+    }
+}
