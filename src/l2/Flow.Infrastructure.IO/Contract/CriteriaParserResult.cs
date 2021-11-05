fd5a88f9d95fc913ec00f27e6f471d69d4f4a91a
Nikita Senkov <nvsnkv@live.com>
2021-11-05 23:54:13 +0300

    Criteria parser

---
+++ src/l2/Flow.Infrastructure.IO/Contract/CriteriaParserResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using Flow.Domain.Transactions;
+
+namespace Flow.Infrastructure.IO.Contract;
+
+public class CriteriaParserResult<T> where T: Transaction
+{
+    public CriteriaParserResult(Expression<Func<T, bool>>? conditions, IEnumerable<string> errors)
+    {
+        Conditions = conditions;
+        Errors = errors;
+    }
+
+    public Expression<Func<T, bool>>? Conditions { get; }
+
+    public IEnumerable<string> Errors { get; }
+
+    public bool Successful => Conditions != null;
+}
---
+++ src/l2/Flow.Infrastructure.IO/Contract/ITransactionCriteriaParser.cs
@@ -0,0 +1,10 @@
+﻿using Flow.Domain.Transactions;
+
+namespace Flow.Infrastructure.IO.Contract;
+
+public interface ITransactionCriteriaParser
+{
+    CriteriaParserResult<Transaction> ParseTransactionCriteria(IEnumerable<string> parts);
+
+    CriteriaParserResult<RecordedTransaction> ParseRecordedTransactionCriteria(IEnumerable<string> parts);
+}
