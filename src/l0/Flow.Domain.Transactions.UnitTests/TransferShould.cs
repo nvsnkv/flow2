e7e3a54f338b6f41fe9841f08b91a60726b8fdc4
Nikita Senkov <nvsnkv@live.com>
2021-11-08 10:12:15 +0300

    transfer domain object

---
+++ src/l0/Flow.Domain.Transactions.UnitTests/TransferShould.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Flow.Domain.Transactions.Transfers;
+using FluentAssertions;
+using Xunit;
+using Xunit.Categories;
+
+namespace Flow.Domain.Transactions.UnitTests;
+
+public class TransferShould
+{
+    private readonly RecordedTransaction source = new(1, DateTime.UtcNow, -100, "RUR", null, "Transfer");
+    private readonly RecordedTransaction anotherSource = new(5, DateTime.UtcNow, -100, "RUR", null, "Transfer");
+    private readonly RecordedTransaction sink = new(2, DateTime.UtcNow, 100, "RUR", null, "Transfer");
+    private readonly RecordedTransaction sinkWithFee = new(3, DateTime.UtcNow, 90, "RUR", null, "Transfer");
+    private readonly RecordedTransaction sinkWithDifferentCurrency = new(4, DateTime.UtcNow, 90, "EUR", null, "Transfer");
+
+    [Fact] [UnitTest]
+    public void ThrowArgumentExceptionIfSourceAndSinkAreTheSame()
+    {
+        Action a = () =>
+        {
+            var _ = new Transfer(0, 0, 0, "");
+        };
+
+        a.Should().Throw<ArgumentException>().Where(e => e.ParamName == "sink");
+    }
+
+    [Fact] [UnitTest]
+    public void ThrowArgumentExceptionIfTransactionsAreInDifferentCurrencies()
+    {
+        Action a = () =>
+        {
+            var _ = new Transfer(source, sinkWithDifferentCurrency);
+        };
+
+        a.Should().Throw<ArgumentException>().Where(e => e.ParamName == "sink");
+    }
+
+    [Fact] [UnitTest]
+    public void ThrowArgumentExceptionIfTransactionsIfSourceHasPositiveAmount()
+    {
+        Action a = () =>
+        {
+            var _ = new Transfer(sinkWithFee, sink);
+        };
+
+        a.Should().Throw<ArgumentException>().Where(e => e.ParamName == "source");
+    }
+
+    [Fact] [UnitTest]
+    public void ThrowArgumentExceptionIfTransactionsIfSinkHasNegativeAmount()
+    {
+        Action a = () =>
+        {
+            var _ = new Transfer(source, anotherSource);
+        };
+
+        a.Should().Throw<ArgumentException>().Where(e => e.ParamName == "sink");
+    }
+
+    [Fact] [UnitTest]
+    public void CalculateFeeAsDifferenceBetweenSourceAndSink()
+    {
+        var transfer = new Transfer(source, sinkWithFee);
+        transfer.Fee.Should().Be(source.Amount + sinkWithFee.Amount);
+        transfer.Currency.Should().Be(source.Currency);
+    }
+
+    [Fact] [UnitTest]
+    public void PopulateSourceAndSinkProperly()
+    {
+        var transfer = new Transfer(source, sink);
+        transfer.Source.Should().Be(source.Key);
+        transfer.Sink.Should().Be(sink.Key);
+    }
+}
---
+++ src/l0/Flow.Domain.Transactions/Transfers/Transfer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Flow.Domain.Transactions.Transfers;
+
+public class Transfer
+{
+    public Transfer(long source, long sink, decimal fee, string currency)
+    {
+        if (source == sink) throw new ArgumentException("Source and sink must be different!", nameof(sink));
+
+        Source = source;
+        Sink = sink;
+        Fee = fee;
+        Currency = currency;
+    }
+
+    public Transfer(RecordedTransaction source, RecordedTransaction sink)
+    {
+        if (source.Key == sink.Key) throw new ArgumentException("Source and sink must be different!", nameof(sink));
+        if (source.Currency != sink.Currency) throw new ArgumentException("Unable to create transfer object - currencies are different!", nameof(sink));
+        if (source.Amount >= 0) throw new ArgumentException("Amount must be negative for source!", nameof(source));
+        if (sink.Amount < 0) throw new ArgumentException("Amount must not be negative for sink!", nameof(sink));
+
+        Source = source.Key;
+        Sink = sink.Key;
+        Fee = source.Amount + sink.Amount;
+        Currency = source.Currency;
+    }
+
+    public long Source { get; }
+
+    public long Sink { get; }
+
+    public decimal Fee { get; }
+
+    public string Currency { get; }
+}
