482afd5676f051ef652eeab7900de9bc6d312d2c
Nikita Senkov <nvsnkv@live.com>
2021-11-12 09:50:02 +0300

    separated interfaces

---
+++ src/l2/Flow.Infrastructure.IO/Contract/IRejectionsWriter.cs
@@ -0,0 +1,9 @@
+﻿using Flow.Domain.Transactions;
+using Flow.Infrastructure.Configuration.Contract;
+
+namespace Flow.Infrastructure.IO.Contract;
+
+public interface IRejectionsWriter
+{
+    Task WriteRejections(StreamWriter writer, IEnumerable<RejectedTransaction> rejections, SupportedFormat format, CancellationToken ct);
+}
--- src/l2/Flow.Infrastructure.IO/Contract/ITransactionsReader.cs
+++ src/l2/Flow.Infrastructure.IO/Contract/ITransactionsReader.cs
@@ -8,15 +8,3 @@ public interface ITransactionsReader
 
     Task<IEnumerable<RecordedTransaction>> ReadRecordedTransactions(StreamReader reader, SupportedFormat format, CancellationToken ct);
 }
-
-public interface ITransactionsWriter
-{
-    Task WriteTransactions(StreamWriter writer, IEnumerable<Transaction> transactions, SupportedFormat format, CancellationToken ct);
-
-    Task WriteRecordedTransactions(StreamWriter writer, IEnumerable<RecordedTransaction> transactions, SupportedFormat format, CancellationToken ct);
-}
-
-public interface IRejectionsWriter
-{
-    Task WriteRejections(StreamWriter writer, IEnumerable<RejectedTransaction> rejections, SupportedFormat format, CancellationToken ct);
-}
