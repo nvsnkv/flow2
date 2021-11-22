39328aaa348ae8806b0ef003ef9aceb8b74bfc2e
Nikita Senkov <nvsnkv@live.com>
2021-11-22 10:08:37 +0300

    key analysis domain entities

---
+++ src/l0/Flow.Domain.Analysis/Expense.cs
@@ -0,0 +1,11 @@
+﻿using Flow.Domain.Transactions;
+
+namespace Flow.Domain.Analysis;
+
+public class Expense : FlowItem
+{
+    public Expense(RecordedTransaction transaction) : base(transaction)
+    {
+        if (transaction.Amount > 0) { throw new ArgumentException("Cannot create an expense from transaction with positive amount!", nameof(transaction)); }
+    }
+}
---
+++ src/l0/Flow.Domain.Analysis/Income.cs
@@ -0,0 +1,11 @@
+﻿using Flow.Domain.Transactions;
+
+namespace Flow.Domain.Analysis;
+
+public class Income : FlowItem
+{
+    public Income(RecordedTransaction transaction) : base(transaction)
+    {
+        if (transaction.Amount < 0) { throw new ArgumentException("Cannot create income from transaction with negative amount!", nameof(transaction)); }
+    }
+}
