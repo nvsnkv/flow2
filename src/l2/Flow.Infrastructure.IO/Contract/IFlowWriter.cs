931f05ec46c6648ac3a51ba468fcf6454d8c0521
Nikita Senkov <nvsnkv@live.com>
2021-11-29 14:10:51 +0300

    Revert "Flow items writer"
    This reverts commit b19a839a0f080c6686a54ec97fb6e20e2cf34a56.

--- src/l2/Flow.Infrastructure.IO/Contract/IFlowWriter.cs
+++
@@ -1,9 +0,0 @@
-using Flow.Domain.Analysis;
-using Flow.Infrastructure.Configuration.Contract;
-
-namespace Flow.Infrastructure.IO.Contract;
-
-public interface IFlowWriter 
-{
-    Task Write(StreamWriter writer, IAsyncEnumerable<FlowItem> items, SupportedFormat format, CancellationToken ct);
-}
