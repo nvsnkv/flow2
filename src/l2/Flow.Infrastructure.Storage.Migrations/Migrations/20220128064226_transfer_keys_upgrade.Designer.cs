99c3e3cf1b6d3ac8bc1cdd2ca733974184f39990
Nikita Senkov <nvsnkv@live.com>
2022-01-28 09:43:35 +0300

    added db migration

--- src/l2/Flow.Infrastructure.Storage.Migrations/Migrations/20220128064226_transfer_keys_upgrade.Designer.cs
+++ src/l2/Flow.Infrastructure.Storage.Migrations/Migrations/20220128064226_transfer_keys_upgrade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Flow.Infrastructure.Storage.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Flow.Infrastructure.Storage.Migrations.Migrations
 {
     [DbContext(typeof(FlowDbContext))]
-    partial class FlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220128064226_transfer_keys_upgrade")]
+    partial class transfer_keys_upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
