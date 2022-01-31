36e4ae0d023def177532d31fca5c64ca9241bf77
Nikita Senkov <nvsnkv@live.com>
2022-01-31 09:28:37 +0300

    fuzzy transfres detector & tests

---
+++ src/l1/Flow.Application.Transactions.UnitTests/FuzzyTransferDetectorShould.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Flow.Application.ExchangeRates.Contract;
+using Flow.Application.Transactions.Transfers;
+using Flow.Domain.Transactions;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using Xunit.Categories;
+
+namespace Flow.Application.Transactions.UnitTests;
+
+public class FuzzyTransferDetectorShould
+{
+    [Fact, UnitTest]
+    public void NotThreatPairAsTransferIfSourceIsPositive()
+    {
+        var left = new RecordedTransaction(0, DateTime.UtcNow, 100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.UtcNow, 100, "ZBW", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeFalse();
+    }
+
+    [Fact, UnitTest]
+    public void NotThreatPairAsTransferIfSourceDoesNotMatchSink()
+    {
+        var left = new RecordedTransaction(0, DateTime.UtcNow, -100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.UtcNow, 105, "ZBW", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeFalse();
+    }
+
+    [Fact, UnitTest]
+    public void NotThreatPairAsTransferIfCurrencyDiffers()
+    {
+        var left = new RecordedTransaction(0, DateTime.UtcNow, -100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.UtcNow, 100, "BYN", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeFalse();
+    }
+
+    [Theory, UnitTest]
+    [InlineData("2022-01-30", 4)]
+    [InlineData("2022-01-28", 5)]
+    [InlineData("2022-01-28", 10)]
+    [InlineData("2022-01-28", 1000)]
+    public void NotThreatPairAsTransferIfTimespanIsGreaterThenThreeBusinessDays(string timespan, int offset)
+    {
+        var left = new RecordedTransaction(0, DateTime.Parse(timespan), -100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.Parse(timespan).AddDays(offset), 100, "ZBW", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeFalse();
+    }
+
+    [Theory, UnitTest]
+    [InlineData("2022-01-31", "2022-01-31")]
+    [InlineData("2022-01-31", "2022-01-30")]
+    [InlineData("2022-01-31", "2022-01-29")]
+    [InlineData("2022-01-30", "2022-01-30")]
+    [InlineData("2022-01-30", "2022-01-29")]
+    public void DetectTransfersMadeOverTheWeekend(string sourceTimestamp, string sinkTimestamp)
+    {
+        var left = new RecordedTransaction(0, DateTime.Parse(sourceTimestamp), -100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.Parse(sinkTimestamp), 100, "ZBW", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeTrue();
+    }
+
+    [Theory, UnitTest]
+    [InlineData("2022-01-28", "2022-01-31")]
+    [InlineData("2022-01-29", "2022-01-31")]
+    [InlineData("2022-01-30", "2022-01-31")]
+    [InlineData("2022-01-31", "2022-01-31")]
+    [InlineData("2022-01-28", "2022-02-01")]
+    [InlineData("2022-01-29", "2022-02-01")]
+    [InlineData("2022-01-30", "2022-02-01")]
+    [InlineData("2022-01-31", "2022-02-01")]
+    [InlineData("2022-01-28", "2022-02-02")]
+    [InlineData("2022-01-29", "2022-02-02")]
+    [InlineData("2022-01-30", "2022-02-02")]
+    [InlineData("2022-01-31", "2022-02-02")]
+    [InlineData("2022-01-31", "2022-02-03")]
+    public void DetectTransfersMadeWithinThreeBusinessDays(string sourceTimestamp, string sinkTimestamp)
+    {
+        var left = new RecordedTransaction(0, DateTime.Parse(sourceTimestamp), -100, "ZBW", null, "Title", new("Account", "Bank"));
+        var right = new RecordedTransaction(1, DateTime.Parse(sinkTimestamp), 100, "ZBW", null, "Title", new("Account", "Bank"));
+
+        new FuzzyTransferDetector(new Mock<IExchangeRatesProvider>().Object).CheckIsTransfer(left, right).Should().BeTrue();
+    }
+}
---
+++ src/l1/Flow.Application.Transactions/Transfers/FuzzyTransferDetector.cs
@@ -0,0 +1,43 @@
+﻿using Flow.Application.ExchangeRates.Contract;
+using Flow.Domain.Transactions;
+using Flow.Domain.Transactions.Transfers;
+using FluentDateTime;
+
+namespace Flow.Application.Transactions.Transfers;
+
+class FuzzyTransferDetector : TransferDetectorBase
+{
+    private static readonly int TransferWindowDays = 3;
+    public FuzzyTransferDetector(IExchangeRatesProvider ratesProvider) : base("May be a transfer", ratesProvider, DetectionAccuracy.Likely)
+    {
+    }
+
+    public override bool CheckIsTransfer(RecordedTransaction left, RecordedTransaction right)
+    {
+        if (left.Amount > 0 || -1 * left.Amount != right.Amount)
+        {
+            return false;
+        }
+
+        if (left.Account == right.Account)
+        {
+            return false;
+        }
+
+        var leftTs = GetBusinessDate(left.Timestamp);
+        var rightTs = GetBusinessDate(right.Timestamp);
+        
+
+        return leftTs <= rightTs && rightTs <= leftTs.AddBusinessDays(TransferWindowDays);
+    }
+
+    private static DateTime GetBusinessDate(DateTime timestamp)
+    {
+        return timestamp.DayOfWeek switch
+        {
+            DayOfWeek.Sunday => timestamp.Date.AddDays(1),
+            DayOfWeek.Saturday => timestamp.Date.AddDays(2),
+            _ => timestamp.Date
+        };
+    }
+}
