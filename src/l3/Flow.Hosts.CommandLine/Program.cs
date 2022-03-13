1099bf6a1cc14c69341cc89167b1c5236718d0c4
Nikita Senkov <nvsnkv@live.com>
2022-03-13 22:10:12 +0300

    PoC

---
+++ src/l3/Flow.Hosts.CommandLine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+
+
+var root = new RootCommand("Flow CLI")
+{
+    new Command("accounting", "Transactions management")
+    {
+        new Command("list"),
+        new Command("add"),
+        new Command("update"),
+        new Command("delete"),
+        new Command("find-duplicates")
+    },
+    new Command("build", "Money flow analysis"),
+    new Command("rates", "Exchange rates handling"),
+    new Command("transfers", "Transfers adjustment")
+};
+
+return await root.InvokeAsync(args);
