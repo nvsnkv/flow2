4c0ce88d8ce65e207b1506fc744ca20f6e44f022
Nikita Senkov <nvsnkv@live.com>
2022-09-02 21:16:18 +0300

    fixes n tweaks

--- src/l3/CommandLine/Flow.Hosts.EntryPoint.Cli/Commands/RatesCommand.cs
+++ src/l3/CommandLine/Flow.Hosts.EntryPoint.Cli/Commands/RatesCommand.cs
@@ -3,4 +3,4 @@
 namespace Flow.Hosts.EntryPoint.Cli.Commands;
 
 [Verb("rates", HelpText = "Invokes rates management module. Please refer to `rates help` output for details")]
-internal sealed class RatesCommand : CommandWithRequiredArgs { }
+internal sealed class RatesCommand : CommandWithOptionalArgs { }
--- src/l3/CommandLine/Flow.Hosts.EntryPoint.Cli/Commands/XfersCommand.cs
+++ src/l3/CommandLine/Flow.Hosts.EntryPoint.Cli/Commands/XfersCommand.cs
@@ -3,4 +3,4 @@
 namespace Flow.Hosts.EntryPoint.Cli.Commands;
 
 [Verb("xfers", HelpText = "Invokes transfers management module. Please refer to `xfers help` output for details")]
-internal sealed class XfersCommand : CommandWithRequiredArgs { }
+internal sealed class XfersCommand : CommandWithOptionalArgs { }
