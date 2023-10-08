f6d80bb5495934fac112bc248a977f8af4f1a804
Nikita Senkov <nvsnkv@live.com>
2023-10-08 16:23:29 +0300

    chore:  minor cleanup

--- src/plugins/transactions/Flow.Plugins.Transactions.TKF/TransfersDetector.cs
+++ src/plugins/transactions/Flow.Plugins.Transactions.TKF/TransfersDetector.cs
@@ -1,33 +1,31 @@
 ﻿using Flow.Domain.Transactions;
 using Flow.Domain.Transactions.Transfers;
 using Flow.Infrastructure.Plugins.Transactions.Contract;
-using System.Xml.Linq;
 
-namespace Flow.Plugins.Transactions.TKF
+namespace Flow.Plugins.Transactions.TKF;
+
+public sealed class TransfersDetector : ITransferDetectionPlugin
 {
-    public sealed class TransfersDetector : ITransferDetectionPlugin
+    public bool CheckIsTransfer(RecordedTransaction left, RecordedTransaction right)
     {
-        public bool CheckIsTransfer(RecordedTransaction left, RecordedTransaction right)
-        {
-            return left.Timestamp == right.Timestamp
-                   && left.Amount < 0
-                   && right.Amount + left.Amount == 0
-                   && left.Category == "Переводы/иб"
-                   && left.Title == "Перевод между счетами"
-                   && right.Category == "Другое"
-                   && right.Title == "Перевод между счетами";
-        }
-
-        public Task<Transfer> Create(RecordedTransaction left, RecordedTransaction right, CancellationToken ct)
-        {
-            var result = new Transfer(left, right, DetectionAccuracy.Exact)
-            {
-                Comment = "Перевод между счетами ТКФ"
-            };
-
-            return Task.FromResult(result);
-        }
-
-        public DetectionAccuracy Accuracy => DetectionAccuracy.Exact;
+        return left.Timestamp == right.Timestamp
+               && left.Amount < 0
+               && right.Amount + left.Amount == 0
+               && left.Category == "Переводы/иб"
+               && left.Title == "Перевод между счетами"
+               && right.Category == "Другое"
+               && right.Title == "Перевод между счетами";
     }
+
+    public Task<Transfer> Create(RecordedTransaction left, RecordedTransaction right, CancellationToken ct)
+    {
+        var result = new Transfer(left, right, DetectionAccuracy.Exact)
+        {
+            Comment = "Перевод между счетами ТКФ"
+        };
+
+        return Task.FromResult(result);
+    }
+
+    public DetectionAccuracy Accuracy => DetectionAccuracy.Exact;
 }
