327a0cbff8862a5bb140ca3f8200831be773df9b
Nikita Senkov <nvsnkv@live.com>
2023-10-18 10:05:46 +0300

    feat: flexible transactions reader plugin

---
+++ src/plugins/transactions/Flow.Plugins.Transactions.FlexibleTransactionsReader/Flow.Plugins.Transactions.FlexibleTransactionsReader/IFlexibleTransactionsReader.cs
@@ -0,0 +1,7 @@
+﻿using Flow.Domain.Transactions;
+using Flow.Infrastructure.IO.Contract;
+using Flow.Infrastructure.Plugins.Contract;
+
+namespace Flow.Plugins.Transactions.FlexibleTransactionsReader;
+
+public interface IFlexibleTransactionsReader : IPlugin, IFormatSpecificReader<(Transaction, Overrides)> { }
---
+++ src/plugins/transactions/Flow.Plugins.Transactions.FlexibleTransactionsReader/Flow.Plugins.Transactions.FlexibleTransactionsReader/Settings/MappingRule.cs
@@ -0,0 +1,10 @@
+﻿namespace Flow.Plugins.Transactions.FlexibleTransactionsReader.Settings;
+
+internal sealed class MappingRule
+{
+    public string Field { get; set; }
+
+    public int? ColumnId { get; set; }
+
+    public string? FallbackValue { get; set; }
+}
